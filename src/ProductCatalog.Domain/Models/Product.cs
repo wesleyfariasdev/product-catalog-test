cf2feab902f036fe2ad3bce4f3b7d9e9c79f5033
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 01:27:37 -0300

    Adds constructors to the Product class: includes a parameterless constructor and a constructor with parameters to support mapping with AutoMapper.

--- src/ProductCatalog.Domain/Models/Product.cs
+++ src/ProductCatalog.Domain/Models/Product.cs
@@ -2,13 +2,32 @@
 
 namespace ProductCatalog.Domain.Models
 {
-    public sealed class Product(int id, string name, decimal price, string description, ProductType productType, DateTime registrationDate)
+    public sealed class Product
     {
-        public int Id { get; } = id;
-        public string Name { get; } = name;
-        public decimal Price { get; } = price;
-        public string Description { get; } = description;
-        public ProductType ProductType { get; } = productType;
-        public DateTime RegistrationDate { get; } = registrationDate;
+        public Product(int id, string name, decimal price, string description, ProductType productType, DateTime registrationDate)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = description;
+            ProductType = productType;
+            RegistrationDate = registrationDate;
+        }
+
+        public Product(string name, decimal price, string description, ProductType productType, DateTime registrationDate)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            ProductType = productType;
+            RegistrationDate = registrationDate;
+        } 
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+        public ProductType ProductType { get; private set; }
+        public DateTime RegistrationDate { get; private set; }
     }
 }

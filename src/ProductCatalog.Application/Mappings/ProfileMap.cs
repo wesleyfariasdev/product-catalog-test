4aa5e63aa66823e63d7ba4a439135655f77dd970
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 00:31:49 -0300

    feat(application): add AutoMapper profile for mapping between Product and DTOs

---
+++ src/ProductCatalog.Application/Mappings/ProfileMap.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductCatalog.Application.Dto.Request;
+using ProductCatalog.Application.Dto.Response;
+using ProductCatalog.Domain.Models;
+
+namespace ProductCatalog.Application.Mappings
+{
+    public class ProfileMap : Profile
+    {
+        public ProfileMap()
+        {
+            CreateMap<Product, ProductRequestDTO>().ReverseMap();
+            CreateMap<Product, ProductResponseDto>().ReverseMap();
+        }
+    }
+}

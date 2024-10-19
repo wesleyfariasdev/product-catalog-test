a956924c251c623f408367989b57e10dbc571f8d
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 00:42:10 -0300

    chore(ioc): register ProductService and IProductService in dependency injection container

--- src/ProductCatalog.Infra.IoC/IoC.cs
+++ src/ProductCatalog.Infra.IoC/IoC.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Application.Services;
+using ProductCatalog.Application.Services.IServices;
 using ProductCatalog.Domain.Interface;
 using ProductCatalog.Infra.Data.Repository;
 
@@ -9,6 +11,7 @@ namespace ProductCatalog.Infra.IoC
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<IProductRepository, ProductRepository>();
+            services.AddSingleton<IProductServices, ProductServices>();
             return services;
         }
     }

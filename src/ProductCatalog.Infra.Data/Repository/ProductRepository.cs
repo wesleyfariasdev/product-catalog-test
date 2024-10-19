94ea19288870eb1f80c0799ea72b15cf3992b6ae
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 02:02:02 -0300

    feat: Implement product ID incrementation in ProductRepository

--- src/ProductCatalog.Infra.Data/Repository/ProductRepository.cs
+++ src/ProductCatalog.Infra.Data/Repository/ProductRepository.cs
@@ -6,8 +6,11 @@ namespace ProductCatalog.Infra.Data.Repository
     public class ProductRepository(List<Product> initialProducts) : IProductRepository
     {
         private readonly List<Product> _products = initialProducts;
+        private static int _nextId = 1;
+
         public Task<Product> CreateProduct(Product product)
         {
+            product = new Product(_nextId++, product.Name, product.Price, product.Description, product.ProductType, DateTime.UtcNow);
             _products.Add(product);
             return Task.FromResult(product);
         }

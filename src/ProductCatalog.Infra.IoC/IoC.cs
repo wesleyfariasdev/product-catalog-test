7071695bc0e12997fb46cadbd0ae2b76f4f64361
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 00:08:11 -0300

    feat(IoC): configure dependency injection

---
+++ src/ProductCatalog.Infra.IoC/IoC.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Domain.Interface;
+using ProductCatalog.Infra.Data.Repository;
+
+namespace ProductCatalog.Infra.IoC
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductRepository, ProductRepository>();
+            return services;
+        }
+    }
+}

c08367ca1a4a4cbc92a9a7ff3c1a9c2649a9ac7d
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 01:29:56 -0300

    Updates dependency injection configuration: adds singleton for product list and registers AutoMapper.

--- src/ProductCatalog.Infra.IoC/IoC.cs
+++ src/ProductCatalog.Infra.IoC/IoC.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Application.Mappings;
 using ProductCatalog.Application.Services;
 using ProductCatalog.Application.Services.IServices;
 using ProductCatalog.Domain.Interface;
+using ProductCatalog.Domain.Models;
 using ProductCatalog.Infra.Data.Repository;
 
 namespace ProductCatalog.Infra.IoC
@@ -10,8 +12,13 @@ namespace ProductCatalog.Infra.IoC
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddSingleton<IProductRepository, ProductRepository>();
-            services.AddSingleton<IProductServices, ProductServices>();
+            services.AddSingleton(new List<Product>());
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductServices, ProductServices>();
+
+            services.AddAutoMapper(typeof(ProfileMap));
+
             return services;
         }
     }

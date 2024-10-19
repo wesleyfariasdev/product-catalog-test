362ce4f7575f62463cf1b471f10fc20081e6a424
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 00:39:29 -0300

    feat(application): add IProductService interface and ProductService implementation

---
+++ src/ProductCatalog.Application/Services/IServices/IProductServices.cs
@@ -0,0 +1,11 @@
+﻿using ProductCatalog.Application.Dto.Request;
+using ProductCatalog.Application.Dto.Response;
+
+namespace ProductCatalog.Application.Services.IServices
+{
+    public interface IProductServices
+    {
+        Task<IEnumerable<ProductResponseDto>> GetProductsAsync();
+        Task<ProductResponseDto> CreateProduct(ProductRequestDTO product);
+    }
+}
---
+++ src/ProductCatalog.Application/Services/ProductServices.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ProductCatalog.Application.Dto.Request;
+using ProductCatalog.Application.Dto.Response;
+using ProductCatalog.Application.Services.IServices;
+using ProductCatalog.Domain.Interface;
+using ProductCatalog.Domain.Models;
+
+namespace ProductCatalog.Application.Services
+{
+    public class ProductServices(IMapper mapper, IProductRepository productRepository) : IProductServices
+    {
+        public async Task<ProductResponseDto> CreateProduct(ProductRequestDTO productRequest)
+        {
+            var product = mapper.Map<Product>(productRequest);
+            return mapper.Map<ProductResponseDto>(await productRepository.CreateProduct(product));
+        }
+
+        public async Task<IEnumerable<ProductResponseDto>> GetProductsAsync()
+        {
+            var products = await productRepository.GetProductsAsync();
+            return mapper.Map<IEnumerable<ProductResponseDto>>(products);
+        }
+    }
+}

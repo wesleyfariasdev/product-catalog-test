7a0b827070b397f03e4ece320a2f3713483d007a
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 00:05:50 -0300

    feat(infrastructure): implement ProdutoRepository with in-memory data

---
+++ src/ProductCatalog.Infra.Data/Repository/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProductCatalog.Domain.Interface;
+using ProductCatalog.Domain.Models;
+
+namespace ProductCatalog.Infra.Data.Repository
+{
+    public class ProductRepository(List<Product> initialProducts) : IProductRepository
+    {
+        private readonly List<Product> _products = initialProducts;
+        public Task<Product> CreateProduct(Product product)
+        {
+            _products.Add(product);
+            return Task.FromResult(product);
+        }
+
+        public Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return Task.FromResult(_products.AsEnumerable());
+        }
+    }
+}

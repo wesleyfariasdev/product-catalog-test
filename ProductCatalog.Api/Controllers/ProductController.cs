11af84f4ad06545305322622764a4f6eab80e326
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-19 01:32:18 -0300

    Creates API project and implements ProductController for managing product operations

---
+++ ProductCatalog.Api/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Application.Dto.Request;
+using ProductCatalog.Application.Services.IServices;
+
+namespace ProductCatalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IProductServices productService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductRequestDTO productRequest)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var productResponse = await productService.CreateProductAsync(productRequest);
+            return CreatedAtAction(nameof(GetAllProducts), new { id = productResponse.Id }, productResponse);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await productService.GetProductsAsync();
+            return Ok(products);
+        }
+    }
+}

5ae27595323402d47eabf05d582ddf8c00b0c63d
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-22 21:25:22 -0300

    feat: implement ProductService for API integration

---
+++ ProductCatalog.Web/Services/ProductServices.cs
@@ -0,0 +1,32 @@
+﻿using ProductCatalog.Web.Response;
+using System.Text.Json;
+
+namespace ProductCatalog.Web.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ProductResponse> GetProductsAsync(int page = 1, int pageSize = 5)
+        {
+            var response = await _httpClient.GetAsync($"https://localhost:7113/api/product?page={page}&pageSize={pageSize}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ProductResponse>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+
+            throw new Exception("Erro ao carregar produtos");
+        }
+    }
+
+}

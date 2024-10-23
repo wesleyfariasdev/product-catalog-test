60de25291942058d4af50c19dc81e9b8ffe62c2b
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-22 23:01:29 -0300

    refactor: change properties in DTO from init to set

--- src/ProductCatalog.Application/Dto/Request/ProductRequestDto.cs
+++ src/ProductCatalog.Application/Dto/Request/ProductRequestDto.cs
@@ -9,24 +9,24 @@ public sealed class ProductRequestDTO
     [JsonPropertyName("name")]
     [Required(ErrorMessage = "O nome do produto é obrigatório.")]
     [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
-    public string Name { get; init; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
     [JsonPropertyName("price")]
     [Required(ErrorMessage = "O preço é obrigatório.")]
     [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
-    public decimal Price { get; init; }
+    public decimal Price { get; set; }
 
     [JsonPropertyName("description")]
     [StringLength(500, ErrorMessage = "A descrição deve ter no máximo 500 caracteres.")]
-    public string? Description { get; init; }
+    public string? Description { get; set; }
 
     [JsonPropertyName("product_type")]
     [Required(ErrorMessage = "O tipo do produto é obrigatório.")]
-    public ProductType ProductType { get; init; }
+    public ProductType ProductType { get; set; }
 
     [JsonPropertyName("registration_date")]
     [Required(ErrorMessage = "A data de registro é obrigatória.")]
-    public DateTime RegistrationDate { get; init; }
+    public DateTime RegistrationDate { get; set; }
 
 }
 

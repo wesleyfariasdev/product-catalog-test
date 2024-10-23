067bc3da9240f8c730ddbc93850995872f3c7596
wesleyfarias.dev@gmail.com <wesleyfarias.dev@gmail.com>
2024-10-22 21:25:12 -0300

    feat: add ProductResponse

---
+++ ProductCatalog.Web/Response/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿using ProductCatalog.Application.Dto.Response;
+
+namespace ProductCatalog.Web.Response
+{
+    public class ProductResponse
+    {
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public IEnumerable<ProductResponseDto> Products { get; set; }
+    }
+}
